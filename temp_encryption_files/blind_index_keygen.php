001261274658066004d0ff86cc5999370365634a
Nicholas Weiss <nsweiss@ualberta.ca>
2025-10-28 10:09:22 -0600

    Removed temporary encryption files

--- temp_encryption_files/blind_index_keygen.php
+++
@@ -1,6 +0,0 @@
-<?php
-$new_key = random_bytes(SODIUM_CRYPTO_SECRETBOX_KEYBYTES);
-
-$encoded_key_for_storage = sodium_bin2base64($new_key, SODIUM_BASE64_VARIANT_ORIGINAL);
-
-echo $encoded_key_for_storage;
